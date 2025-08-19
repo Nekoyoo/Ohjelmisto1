6b58169e3d68144ea6a574e06bd2591e06559fdf
jouni <jounikl93@gmail.com>
2025-08-19 15:19:31 +0300

    b

---
+++ mod2/T2_5.py
@@ -0,0 +1,11 @@
+lei_str = input('Syötä leivisköjen määrä: ')
+nau_str = input('Syötä naulojen määrä: ')
+luo_str = input('Syötä luotien määrä: ')
+lei = float(lei_str)
+nau = float(nau_str)
+luo = float(luo_str)
+lei2g = lei*20*32*13.3
+nau2g = nau*32*13.3
+luo2g = luo*13.3
+kg = (lei2g + nau2g + luo2g)/1000
+print('Kokonaismassa kilogrammoina on: ' + str(kg) + '.')
