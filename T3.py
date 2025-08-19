d84bd8e02fab7303b15257ca097121bdeba84067
Nekoyoo <jounikl93@gmail.com>
2025-08-19 13:46:27 +0300

    Add files via upload

--- T3.py
+++ T3.py
@@ -1,2 +1,6 @@
-säde = input('Syötä ympyrän säde: ')
-print('Ympyrän pinta-ala: ')
+r_str = input('Syötä ympyrän säde: ')
+r = float(r_str)
+pi = 3.14159
+A = pi*r*r
+##laskutoimituksissa ei sekoiteta numeroita ja merkkijonoja
+print('Ympyrän pinta-ala: ' + str(A))
