bafc9d9fe9e837a7efd61ea90de3facae12c9b5a
jouni <jounikl93@gmail.com>
2025-08-24 17:25:35 +0300

    a

--- mod2/T2_5.py
+++ mod2/T2_5.py
@@ -7,5 +7,7 @@ luo = float(luo_str)
 lei2g = lei*20*32*13.3
 nau2g = nau*32*13.3
 luo2g = luo*13.3
-kg = (lei2g + nau2g + luo2g)/1000
-print('Kokonaismassa kilogrammoina on: ' + str(kg) + '.')
+nyky = (lei2g + nau2g + luo2g)/1000
+nyky_kg = int((lei2g + nau2g + luo2g)/1000)
+nyky_g = int(((float(nyky) - float(nyky_kg))*1000))
+print(f'Kokonaismassa nykysuureina on {nyky_kg} kilogrammaa ja {nyky_g} grammaa.')
